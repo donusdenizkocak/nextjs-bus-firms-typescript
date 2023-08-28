e08ec5d74f71c799ed37eff0391fc7a6daad55eb
denizderK <denizderya8668@gmail.com>
2023-08-28 22:50:46 +0300

    nextjs

---
+++ public/Home.tsx
@@ -0,0 +1,12 @@
+import { useRouter } from 'next/router';
+
+const Home = () => {
+  const router = useRouter();
+
+  return (
+    <div>
+      <h1>Ana Sayfa</h1>
+      {/* Ana sayfa içeriği */}
+    </div>
+  );
+};
---
+++ public/Login.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  const handleLogin = () => {
+    // Burada kullanıcı adı ve şifrenin doğru olup olmadığını kontrol edin.
+    // Eğer doğru ise ana sayfaya yönlendirin, değilse hata mesajı gösterin.
+    if (username === 'correctUsername' && password === 'correctPassword') {
+      router.push('/home');
+    } else {
+      alert('Hatalı kullanıcı adı veya şifre.');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <input
+        type="text"
+        placeholder="Kullanıcı Adı"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Şifre"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Giriş Yap</button>
+    </div>
+  );
+};
+
+export default Login;
